ddcf87444595444798de05b5580651b398caacf8
Skoleni <tgrasmanis@gmail.com>
2022-12-12 14:29:59 +0200

    uzdevuma risinājums

---
+++ uzdevums_3.py
@@ -0,0 +1,2 @@
+radiuss = input("Ievadiet radiusu")
+print("Rinka linijas garums ir: ",2*3,14*(radiuss), "centimetri")
---
+++ uzdevums_4.py
@@ -0,0 +1,6 @@
+skaitlis1 = int(input("Ievadiet pirmo skaitli:"))
+skaitlis2 = int(input("Ievadiet otro skaitli:"))
+
+print("Abu skaitlu summa ir ", skaitlis1+skaitlis2)
+print("Abu skaitlu reizinajums ir ", skaitlis1*skaitlis2)
+print("Abu skaitlu dalijums ir ", skaitlis1/skaitlis2)
